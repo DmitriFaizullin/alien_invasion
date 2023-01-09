b031c2f6a0bb67f8971f3973a53deafdc5c2e4c2
Faizullin Dmitri <faizullin-dmitri@mail.ru>
2023-01-09 10:47:49 +0500

    Движение пришельцев

--- settings.py
+++ settings.py
@@ -10,3 +10,15 @@ class Settings():
 
         # Настройки корабля
         self.ship_speed = 1.5
+
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 3
+
+        # Настройки пришельцев
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1  # fleet_direction = 1 обозначает движение вправо; a -1 - влево.
